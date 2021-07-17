65b5d0748c75e96db4d1be458b414f6b68097880
james341 <taine049@gmail.com>
2021-07-17 18:29:22 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -154,6 +154,7 @@ var l_musicDisplay = true;
 // (You can also use external URL for the ogg files)
 var l_musicPlaylist = [
 	{ogg: "killerinp.ogg", name: "Nations - Installation Music"},
+	{ogg: "rewsIntro.ogg", name: "Nations - Installation Music"},
 	{ogg: "flipsidetwo.ogg", name: "Nations - Installation Music"},
 	{ogg: "Riverdenradiotwo.ogg", name: "Nations - Installation Music"},
 	{ogg: "blueprint.ogg", name: "Nations - Installation Music"},
